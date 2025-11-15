65a9064cfbcde46e8ffa101e07eb1e08b97673fc
Roman Bisiuk <roman.bisiuk.shi.2024@lpnu.ua>
2025-11-15 21:01:37 +0200

    Creating models

---
+++ shkandalData/Models/Article.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shkandalData.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public int? ClusterId { get; set; }
+        public Cluster? Cluster { get; set; }
+        public int MediaId { get; set; }
+        public required Media Media { get; set; }
+        public required string Title { get; set; }
+        public string? Link { get; set; }
+        public string? FeaturedImageURL { get; set; }
+        public string? Author { get; set; }
+        public string? Content { get; set; }
+        public string? Status { get; set; }
+        public DateTime? LastViewedAt { get; set; }
+        public int ViewCounter { get; set; }
+        public Pgvector.Vector? Embedding { get; set; }
+    }
+}
---
+++ shkandalData/Models/Cluster.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shkandalData.Models
+{
+    public class Cluster
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public bool IsActive { get; set; }
+
+        public ICollection<Article> Articles { get; set; } = new List<Article>();
+    }
+}
