e96850b10a224d91c1bf397de8497317da81e007
Hrystyna Ivaniv <khrystyna.ivaniv.shi.2024@lpnu.ua>
2026-01-17 18:15:34 +0200

    Implement full flow for Category GetAll and GetById

---
+++ Backend/ShkandalServices/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using ShkandalData.Common;
+using shkandalData.DTOs.ClusterDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShkandalData.Models;
+
+namespace ShkandalServices
+{
+    public interface ICategoryService
+    {
+        public Task<List<Category>> GetAllAsync();
+        public Task<Category?> GetByIdAsync(int id);
+    }
+}
