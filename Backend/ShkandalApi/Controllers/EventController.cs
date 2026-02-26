e5a8b180865eff45d449f5bfc211e4faddd2a2f7
Hrystyna Ivaniv <khrystyna.ivaniv.shi.2024@lpnu.ua>
2026-02-26 18:26:04 +0200

    Add EventContoller. Register interfaces in Program file

---
+++ Backend/ShkandalApi/Controllers/EventController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShkandalData.DTOs.EventDtos;
+using ShkandalServices;
+
+namespace shkandal_api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventController : ControllerBase
+    {
+        private readonly IEventService _service;
+
+        public EventController(IEventService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<EventReadDto>> GetByIdAsync(int id)
+        {
+            var result = await _service.GetByIdAsync(id);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+    }
+}
